addd685052d32041f39e33787a1a91e6d93e4380
gianmarcosilluzio <silluziogianmarco@gmail.com>
2017-09-27 17:28:37 +0200

    SlidingWindowMaxima solution fixed

--- CompetitiveProgramming/code/SlidingWindowMaxima.cpp
+++ CompetitiveProgramming/code/SlidingWindowMaxima.cpp
@@ -4,13 +4,14 @@
  *  Updated on: 26 set 2017
  *      Author: Gianmarco Silluzio
 		Problem: http://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k/0
-		Description Problem: I find the max number in first subarray, and put value in variable max and add it in results array. Then I compare the max value of first array with each next array elements. If array element analyzed is greater than max value, I update max value. For each element I add max to array result.
+		Description Problem: My solution is inspired by https://github.com/rossanoventurini/CompetitiveProgramming/blob/master/notes/lecture2.pdf
 		Time complexity: O(n)
 		Auxiliary Space: O(k)
 */
 
 #include <iostream>
 #include <array>
+#include <deque>
 
 using namespace std;
 
@@ -18,29 +19,33 @@ std::vector<int> getNextLargestNumbers(std::vector<int> array, int k);
 void printArrayElements(std::vector<int> array);
 
 int main() {
-    int k = 5;
-    std::vector<int> array = {3, 7, 4, 5, 8, 12, 7, 6, 17};
+    int k = 2;
+    std::vector<int> array = {5, 3, 2, 4, 6 };
     std::vector<int> results = getNextLargestNumbers(array, k);
     printArrayElements(results);
 }
 
 std::vector<int> getNextLargestNumbers(std::vector<int> array, int k){
     std::vector<int> results;
+    std::deque<int> Q;
     int N = array.size();
     results.reserve(N - k + 1);
-    int max = array[0];
-    for(int i = 0; i < k; i++){
-        if(array[i] > max){
-            max = array [i];
+    for(int i = 0; i < N; i++){
+        while(!Q.empty() && Q.front() <= i-k){
+            Q.pop_front();
         }
-    }
-    results.push_back(max);
-    for(int i = k; i < N; i++){
-        if(array[i] > max){
-            max = array [i];
+        while(!Q.empty() && array[i] > array[Q.back()]){
+            Q.pop_back();
         }
-        results.push_back(max);
+
+        Q.push_back(i);
+
+        if(i >= k-1){
+            results.push_back(array[Q.front()]);
+        }
+
     }
+
     return results;
 }
 
