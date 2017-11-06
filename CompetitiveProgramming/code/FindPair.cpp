9d47c7fccd6ce6c44a171a892762a90113808aa1
gianmarcosilluzio <silluziogianmarco@gmail.com>
2017-11-06 21:12:07 +0100

    Update solution

--- CompetitiveProgramming/code/FindPair.cpp
+++ CompetitiveProgramming/code/FindPair.cpp
@@ -5,25 +5,35 @@
  *      Author: Gianmarco Silluzio
  *      Problem: http://codeforces.com/problemset/problem/160/C?locale=en
  *      Description Solution:
- *      The idea is: sort input elements. Then, find the k-th pair. First element of pair is the position in array elements (sorted) of number given by division of k/n (rounding for excess) less one (because array starts from 0 position). Second element of pair is the position in array elements of number given by subtraction of n (number of elements) moltiplicated the position of first element of pair (calculate before) less 1 from k.
- Time complexity consists of sort array --> O(NlogN)
+ *      Sort input elements. Then, find the k-th pair. First element of pair is the position in array elements (sorted) of number given by division of k/n (rounding for excess) less one (because array starts from 0 position).
+ *      Second element of pair is the position in array elements ((k-1)-minVal*n)/cnt, where minVal is the smaller value than first element, and cnt is the count of first element of array.
+ Time complexity consists of sort array --> O(logN) + O(NlogN)
  Space complexity --> O(N)
  */
 
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <algorithm>
 
 int main() {
-    int n, k;
-    cin >> n;
-    cin >> k;
+    uint64_t n, k, minVal;
+    std::cin >> n;
+    std::cin >> k;
+
     std::vector<int> elements(n);
     for(int i = 0; i < n; i++){
-        cin >> elements[i];
+        std::cin >> elements[i];
     }
     std::sort(elements.begin(), elements.end());
-    int x = ceil(k/(double)n);
-    int y = elements[k-(n*(x-1))-1];
-    std::cout << elements[x-1] << " " << y;
+
+    int x = elements[(k-1)/n];
+
+    for(minVal = 0; minVal < n; minVal++){
+        if(elements[minVal] == x) break;
+    }
+
+    uint64_t cnt = count(elements.begin(), elements.end(), x);
+    int y = elements[((k-1)-minVal*n)/cnt];
+
+    std::cout << x << " " << y;
 }
