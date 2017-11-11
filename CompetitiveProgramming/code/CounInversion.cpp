649361581a81ddf32ddf1cf923a5a211d659a464
gianmarcosilluzio <silluziogianmarco@gmail.com>
2017-11-11 14:34:06 +0100

    First release

---
+++ CompetitiveProgramming/code/CounInversion.cpp
@@ -0,0 +1,90 @@
+/*
+ *  CountInversion.cpp
+ *
+ *  Created on: 07 nov 2017
+ *      Author: Gianmarco Silluzio
+ *      Problem: http://www.spoj.com/problems/INVCNT/
+ *      Description Solution:
+ *      The idea is count all inversions of number during merge step of mergeSort algorithm. The final result is given by the sum of left and rigth count of inversion. The count of inversions is in merge step, in particular when the number of left side is greater than rigth side (the inversions are the middle - position of left number because left and right sides are sorted).
+
+ Time complexity consists of sort array, so the time complexity of merge sort --> O(NlogN)
+ Space complexity --> O(N)
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+vector<T> get_input_sequence(size_t n) {
+    vector<T> sequence(n);
+
+    for(size_t i = 0; i < n; ++i)
+        cin >> sequence[i];
+    return sequence;
+}
+
+uint64_t merge(vector<uint64_t>& arr, int l, int m, int r){
+    int i = l, j = m+1, k = 0;
+    uint64_t inv = 0;
+
+    vector<uint64_t> arr2((r-l)+1);//inspired by Rossano's solution, in my solution I created 2 arrays R and L for copy the content of arr
+
+    while (i <= m && j <= r){
+        if (arr[i] < arr[j]){
+            arr2[k] = arr[i++];
+        }
+        else{
+            arr2[k] = arr[j++];
+            inv += m-i+1;
+        }
+        k++;
+    }
+
+
+    while (i <= m){
+        arr2[k++] = arr[i++];
+    }
+
+    while (j <= r){
+        arr2[k++] = arr[j++];
+    }
+
+    copy(arr2.begin(), arr2.end(), arr.begin()+l);//inspired by Rossano's solution
+
+    return inv;
+}
+
+uint64_t mergeSort(vector<uint64_t>& arr, int l, int r){
+    if (l >= r){
+        return 0;
+    }
+
+    int m = (l+r)/2;
+    uint64_t invL = mergeSort(arr, l, m);
+    uint64_t invR = mergeSort(arr, m+1, r);
+    return invL + invR + merge(arr, l, m, r);
+}
+
+
+int main(){
+    string blank;//"get input" inspired by Rossano's solution
+    size_t n_tests;
+    cin >> n_tests;
+    getline(cin, blank);
+
+    for(size_t i = 0; i < n_tests; ++i) {
+        size_t n;
+        cin >> n;
+        auto A = get_input_sequence<uint64_t>(n);
+        getline(cin, blank);
+        uint64_t inv = mergeSort(A, 0, n-1);
+
+        cout << inv << endl;
+    }
+
+      return 0;
+}
---
+++ CompetitiveProgramming/code/LargestEvenNumber.cpp
@@ -0,0 +1,97 @@
+/*
+ *  LargestEvenNumber.cpp
+ *
+ *  Created on: 09 nov 2017
+ *      Author: Gianmarco Silluzio
+ *      Problem: http://practice.geeksforgeeks.org/problems/largest-even-number/0
+ *      Description Solution:
+ *      The idea is convert input in digit array of number. Order array with counting sort. If the first element of array is even, return result converting array in string (through array in decreasing order). Else, find minimum even value of array, delete it from array and concat to the result (result is obtained converting array in string through array in decreasing order)
+ Time complexity consists of sort array, so the time complexity of counting sort + converting array to string result --> O(n+k) + O(n) where n is the number of elements in input array and k is the range of input.
+Wrost case --> O(n+k) + O(2n);
+ Space complexity --> O(2N)
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> countSort(vector<int> arr);
+vector<int> stringToArray(std::string str);
+std::string maxEven(vector<int> arr);
+
+int main(){
+
+    int numTest = 0;
+    std::cin >> numTest;
+
+    std::string str;
+    for (int i = 0; i < numTest; ++i) {
+        std::cin >> str;
+        str = maxEven(stringToArray(str));
+        std::cout << str << std::endl;
+        str.clear();
+    }
+
+    return 0;
+}
+
+vector<int> countSort(vector<int> arr){
+
+    vector<int> count(10, 0);
+    vector<int> results(arr.size(), 0);
+
+    for (auto i : arr) {
+        count[i]++;
+    }
+
+    for (int i = 0; i < count.size(); i++) {
+        count[i] += count[i-1];
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        results[count[arr[i]]-1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    return results;
+}
+
+vector<int> stringToArray(std::string str){
+    vector<int> arr(str.length());
+
+    for(int i=0;i<str.length();i++){
+        arr[i] = str[i] - '0';
+    }
+
+    return arr;
+}
+
+std::string maxEven(vector<int> arr){
+    arr = countSort(arr);
+    string result;
+    std::pair<int, int> minEven = std::make_pair(-1, -1);
+
+    if(arr[0]%2 != 0){
+        for (int i = arr.size()-1; i >= 0; i--) {
+            if(arr[i]%2 == 0){
+                minEven.first = arr[i];
+                minEven.second = i;
+            }
+        }
+    }
+
+    if(minEven.first != -1){
+        arr[minEven.second] = -1;
+    }
+    for (int i = arr.size()-1; i >= 0; i--) {
+        if(arr[i] != -1){
+            result += to_string(arr[i]);
+        }
+    }
+    if(minEven.first != -1){
+        result += to_string(minEven.first);
+    }
+
+    return result;
+}
